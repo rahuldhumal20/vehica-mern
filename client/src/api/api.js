c8d85f40df696937636aa721939223a44a3507dc
Rahul Dhumal <rahuldhumal2003@gmail.com>
2025-12-28 00:32:51 +0530

    Fix API baseURL env usage

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "import.meta.env.VITE_API_URL"
-});
-
-// Attach token automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
